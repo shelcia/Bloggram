b9cecf69311d60ed95c8f6825a80ac8952dbb596
noob <shelcia1999@gmail.com>
2020-10-11 12:57:00 +0530

    update buttons

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
   return (
     <React.Fragment>
       <Navbar/>
-      <div className="container border" id="container">
+      <div className="container" id="container">
         <div className="row">
           <div className="col-sm-6">
             <img src={BlogLogin} alt="blog" className="img-fluid" />
@@ -18,13 +18,11 @@ const Login = () => {
               <div className="form-group">
                 <label htmlFor="uname">Email:</label>
                 <input type="email" className="form-control" id="uname" placeholder="Enter username" name="uname" required />
-                <div className="valid-feedback">Valid.</div>
-                <div className="invalid-feedback">Please fill out this field.</div>
+                <div className="invalid-feedback">Please enter valid email.</div>
               </div>
               <div className="form-group">
                 <label htmlFor="pwd">Password:</label>
                 <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pswd" required />
-                <div className="valid-feedback">Valid.</div>
                 <div className="invalid-feedback">Please fill out this field.</div>
               </div>
               <div className="text-center">
--- src/components/Login/Signup.jsx
+++ src/components/Login/Signup.jsx
@@ -9,7 +9,7 @@ const Signin = () => {
   return (
     <React.Fragment>
       <Navbar/>
-      <div className="container border" id="container">
+      <div className="container" id="container">
         <div className="row">
           <div className="col-sm-6">
             <img src={BlogLogin} alt="blog" className="img-fluid" />
@@ -20,20 +20,17 @@ const Signin = () => {
                 <div className="form-group">
                 <label htmlFor="name">Name:</label>
                 <input type="text" className="form-control" id="name" placeholder="Enter name" name="name" required />
-                <div className="valid-feedback">Valid.</div>
                 <div className="invalid-feedback">Please fill out this field.</div>
               </div>
               <div className="form-group">
                 <label htmlFor="email">Email:</label>
                 <input type="email" className="form-control" id="email" placeholder="Enter email" name="email" required />
-                <div className="valid-feedback">Valid.</div>
-                <div className="invalid-feedback">Please fill out this field.</div>
+                <div className="invalid-feedback">Please enter valid email.</div>
               </div>
               <div className="form-group">
                 <label htmlFor="pwd">Password:</label>
                 <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pswd" required />
-                <div className="valid-feedback">Valid.</div>
-                <div className="invalid-feedback">Please fill out this field.</div>
+                <div className="invalid-feedback">Your password must contain atleast 6 characters.</div>
               </div>
               <div className="text-center">
                 <button type="submit" class="btn btn-primary">Submit</button>
