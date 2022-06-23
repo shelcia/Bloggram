298b0d26dc498472b89b9205835299442007d2d5
shelcia <shelcia1999@gmail.com>
2022-06-23 23:10:22 +0530

    🐛 fixed bug

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./App";
 import rootReducers from "./redux/reducers";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
 const store = createStore(
   rootReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
