0e3fbb4013596ef32a7b5f042ceb5a4df9fed371
shelcia <shelcia1999@gmail.com>
2023-07-15 19:45:10 +0530

    🐛 layout changes

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -1,12 +1,12 @@
 import { Box, Card, CardContent } from "@mui/material";
 import React from "react";
-import AuthTopbar from "./AuthTopbar";
+// import AuthTopbar from "./AuthTopbar";
 import Footer from "../common/Footer";
 
 const AuthLayout = ({ children, title = "" }) => {
   return (
     <React.Fragment>
-      <AuthTopbar />
+      {/* <AuthTopbar /> */}
       <Box className="container h-100 p-5 d-flex justify-content-center align-items-center">
         <Card>
           <CardContent sx={{ padding: 4 }}>
--- src/pages/home/LandingPage.jsx
+++ src/pages/home/LandingPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 // import Blogs from "./components/Blogs";
 import Hero from "./components/Hero";
-import AuthTopbar from "../../layout/AuthTopbar";
+// import AuthTopbar from "../../layout/AuthTopbar";
 import Footer from "../../common/Footer";
 
 const LandingPage = () => {
   return (
     <React.Fragment>
-      <AuthTopbar />
+      {/* <AuthTopbar /> */}
       <Hero />
       {/* <Blogs /> */}
       <Footer />
